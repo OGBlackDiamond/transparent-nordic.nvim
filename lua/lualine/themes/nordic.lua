dccc339c17783499e8e48a356765022ce537e7a4
Caden <84690305+OGBlackDiamond@users.noreply.github.com>
2025-05-13 13:46:54 -0700

    even more transparency

--- lua/lualine/themes/nordic.lua
+++ lua/lualine/themes/nordic.lua
@@ -5,43 +5,43 @@ local nordic = {}
 nordic.normal = {
     a = { bg = C.orange.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.insert = {
     a = { bg = C.green.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.command = {
     a = { bg = C.cyan.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.visual = {
     a = { bg = C.red.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.replace = {
     a = { bg = C.magenta.bright, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.terminal = {
     a = { bg = C.blue2, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 nordic.inactive = {
     a = { bg = C.gray2, fg = C.black0, gui = 'bold' },
     b = { bg = C.gray1, fg = C.white1 },
-    c = { bg = C.bg_statusline, fg = C.white0 },
+    c = { bg = C.bg_statusline, fg = C.none },
 }
 
 return nordic
